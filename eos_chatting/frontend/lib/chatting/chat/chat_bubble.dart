10a1d18cfd4f634c24f0d7cd0ae57f43ea6619d2
2020042879 <2020042879@hanyang.ac.kr>
2023-05-16 22:15:36 +0900

    eos chatting week7 assignment

--- eos_chatting/frontend/lib/chatting/chat/chat_bubble.dart
+++ eos_chatting/frontend/lib/chatting/chat/chat_bubble.dart
@@ -1,64 +1,49 @@
-import 'package:eos_chatting/config/palette.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final String userName;
 
-  @override
-  State<ChatScreen> createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-
-  void initState() {
-    super.initState();
-  }
+  const ChatBubble(this.message, this.isMe, this.userName, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // TODO : 사진보고 AppBar 만들기
-      appBar: AppBar(
-        leading: const IconButton(
-            onPressed: null,
-            icon: Icon(Icons.arrow_back)
-        ),
-        title: const Center(
-          child: Text('Chat screen'),
-        ),
-        actions: const [
-          IconButton(
-              onPressed: null,
-              icon: Icon(Icons.exit_to_app)
-          ),
-        ],
-        backgroundColor: Palette.facebookColor,
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('/chats/QdjBGESVjwtT0I96tfLG/message').snapshots(),
-        builder: (BuildContext context,
-                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(8.0),
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: isMe ? Colors.lightGreen : Colors.black12,
+                  borderRadius: const BorderRadius.only(),
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.7,
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 16,
+                ),
+                margin: const EdgeInsets.symmetric(
+                  vertical: 4,
+                  horizontal: 8,
+                ),
                 child: Text(
-                  docs[index]['text'],
-                  style: const TextStyle(fontSize: 20.0),
-                )
-              );
-            }
-          );
-        },
-      ),
+                  message,
+                  style: TextStyle(
+                    color: isMe ? Colors.white : Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ eos_chatting/frontend/lib/screens/chat_screen.dart
@@ -0,0 +1,59 @@
+import 'package:eos_chatting/config/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO : 사진보고 AppBar 만들기
+      appBar: AppBar(
+        leading:
+            const IconButton(onPressed: null, icon: Icon(Icons.arrow_back)),
+        title: const Center(
+          child: Text('Chat screen'),
+        ),
+        actions: const [
+          IconButton(onPressed: null, icon: Icon(Icons.exit_to_app)),
+        ],
+        backgroundColor: Palette.facebookColor,
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('/chats/QdjBGESVjwtT0I96tfLG/message')
+            .snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                return Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      docs[index]['text'],
+                      style: const TextStyle(fontSize: 20.0),
+                    ));
+              });
+        },
+      ),
+    );
+  }
+}
